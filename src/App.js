00a9d2c6f79ce5730d3f8d6ed5908162eda34338
DoanHieu <hieuhthor@gmail.com>
2021-12-15 09:37:43 +0700

    add loading

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 import React, {useState,useEffect} from 'react'
 import {Api} from './config/Api';
 import  Tours from './component/Tours'
+import Loading from './component/Loading';
 import './index.css'
 
 export default function App() {
     const [tours, setTours] = useState([]);
+    const [load , setLoad] =useState(false)
     const removeTour = (id) =>{
        const newTours =  tours.filter((tour)=> tour.id !== id);
         setTours(newTours)
@@ -19,31 +21,39 @@ export default function App() {
         try{
             const tours = await axios.get(Api)
             setTours(tours.data)
+            setLoad(true)
         }catch(err){
-            alert(err)
+            alert(err);;
+            setLoad(false)
         }
     }
 
+    if(load){
+        return(
+            <>
+                {(tours.length > 0) ? ( 
+                    <main>
+                        <Tours tours={tours}  removeTour={removeTour}/>
+                    </main>
+                ) : (
+                    <main>
+                        <div className='title'>
+                        <h2>no tours left</h2>
+                        <button className='btn' onClick={takeData}>
+                            refresh
+                        </button>
+                        </div>
+                    </main>
+                )}
+            </>
+        )
+    }else{
+        return(
+            <main>
+                <Loading/>
+            </main>
+        )
+    }
 
-    return (
-        <>
-            {(tours.length > 0) ? ( 
-                <main>
-                     <Tours tours={tours}  removeTour={removeTour}/>
-                </main>
-            ) : (
-                <main>
-                    <div className='title'>
-                    <h2>no tours left</h2>
-                    <button className='btn' onClick={takeData}>
-                        refresh
-                    </button>
-                    </div>
-                 </main>
-            )}
-        </>
-       
-    )
-  
     
 }
